8f9c1882da91485bbdcbff4c99d746f113e6f771
MauF23 <mauriciofuentesw23e@gmail.com>
2024-08-21 10:19:59 -0600

    Update PlayerController.cs

--- Assets/2D Platformer/Scripts/PlayerController.cs
+++ Assets/2D Platformer/Scripts/PlayerController.cs
@@ -50,7 +50,7 @@ namespace Platformer
 			}
 			if (Input.GetKeyDown(KeyCode.Space) && jumpCounter > 0)
 			{
-				float adjustedJumpForce = isGrounded ? jumpForce : jumpForce * 0.65f;
+				float adjustedJumpForce = isGrounded ? jumpForce : jumpForce * 0.55f;
 
 				if (!isGrounded)
 				{
