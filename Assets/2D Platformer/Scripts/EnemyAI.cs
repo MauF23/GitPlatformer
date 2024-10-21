d21c20e7b8f83e42c6dbc7d98f50581b7848af6f
MauF23 <mauriciofuentesw23e@gmail.com>
2024-10-21 14:11:05 -0600

    Fix Movimineto de Enemigos

--- Assets/2D Platformer/Scripts/EnemyAI.cs
+++ Assets/2D Platformer/Scripts/EnemyAI.cs
@@ -8,10 +8,10 @@ namespace Platformer
     {
         public float moveSpeed = 1f; 
         public LayerMask ground;
-        public LayerMask wall;
-
+        public Transform flipSensorOrigin;
+        public float rayWallLenght, rayGroundLenght;
         private Rigidbody2D rigidbody; 
-        public Collider2D triggerCollider;
+
         
         void Start()
         {
@@ -25,16 +25,32 @@ namespace Platformer
 
         void FixedUpdate()
         {
-            if(!triggerCollider.IsTouchingLayers(ground) || triggerCollider.IsTouchingLayers(wall))
-            {
-                Flip();
-            }
-        }
+            FlipSensor();
+		}
         
         private void Flip()
         {
             transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
             moveSpeed *= -1;
         }
+
+        private void FlipSensor()
+        {
+            // Si el sensor no registra el piso
+            if (!Physics2D.Raycast(flipSensorOrigin.position, Vector2.down, rayGroundLenght, ground))
+            {
+                Flip();
+            }
+
+
+            // Si el sensor registra una pared
+			if (Physics2D.Raycast(flipSensorOrigin.position, Vector2.right, rayWallLenght, ground))
+			{
+				Flip();
+			}
+
+            Debug.DrawRay(flipSensorOrigin.position, Vector2.down * rayGroundLenght, Color.green);
+            Debug.DrawRay(flipSensorOrigin.position, Vector2.right * rayWallLenght, Color.red);
+		}
     }
 }
